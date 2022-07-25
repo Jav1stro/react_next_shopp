cf0101fd326538bf1f417929640c42dfd481947c
Javi <javier.amarilla@casademoneda.gob.ar>
2022-07-25 17:36:20 -0300

    Agrego configuraciones, components y page dashboard

---
+++ src/theme/index.js
@@ -0,0 +1,31 @@
+module.exports = {
+    colors: {
+      black: '#000000',
+      white: '#FFFFFF',
+      grayFooter: '#444444',
+      grayPlaceholder: '#5D5D5D',
+      grayIconPlaceholder: '#5E5E5E',
+      grayIcon: '#6C757D',
+      grayBorderInput: '#CED4DA',
+      grayLine: '#EDEDED',
+      grayBackground: '#FAFAFA',
+      violet: '#7749F8',
+      lightViolet: '#b9a1f9',
+      redErrorMessage: '#842029',
+      redError: '#DE1306',
+      redErrorBorder: '#F1AEB5',
+      redErrorBackground: '#F8D7DA',
+      okGreen: '#198754',
+    },
+    fontFamily: {
+      inter: "'Inter', sans-serif",
+      roboto: 'Roboto',
+    },
+    fontWeight: {
+      bold: 700,
+      semiBold: 600,
+      medium: 500,
+      regular: 400,
+    },
+  }
+  
